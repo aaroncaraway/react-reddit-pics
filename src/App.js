e8625589c7aa65f9cd5e7227de698c4e8c6cdea6
aaroncaraway <aaroncaraway42@gmail.com>
2021-01-30 15:50:41 -0800

    Add linter and prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <h1>PARTY TIME</h1>
+      <Dashboard />
     </div>
   );
 }
